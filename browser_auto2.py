957e2d855547bfa4622ce2a5a7df0b5a9e245e24
seraph92 <11453441+seraph92@users.noreply.github.com>
2021-08-15 05:33:33 +0900

    자동화 테스트

---
+++ browser_auto2.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.wait import WebDriverWait
+
+options = Options()
+options.headless = True
+browser = webdriver.Chrome(executable_path="d:/work/chromedriver.exe", options=options)
+browser.get("https://datalab.naver.com/shoppingInsight/sCategory.naver")
+
+time.sleep(3)
+tag_names = browser.find_element_by_css_selector(
+    ".rank_top1000_list"
+).find_elements_by_tag_name("li")
+for tag in tag_names:
+    print(tag.text.split("\n"))
